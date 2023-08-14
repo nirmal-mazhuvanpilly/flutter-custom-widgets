7931f407e719aa6c8c4f9222de4d3ed36ed36a84
nirmal-mazhuvanpilly <nirmalmazhuvanpilly@gmail.com>
2023-08-14 16:19:40 +0530

    Animated Dots

--- lib/animated_dots/animated_dots.dart
+++ lib/animated_dots/animated_dots.dart
@@ -11,29 +11,6 @@ class AnimatedDots extends StatefulWidget {
 }
 
 class _AnimatedDotsState extends State<AnimatedDots> {
-  Offset getOffset(double angle, {double? distance}) {
-    final x = (distance ?? 0) * math.cos(angle * (math.pi / 180));
-    final y = (distance ?? 0) * math.sin(angle * (math.pi / 180));
-    return Offset(x, y);
-  }
-
-  List<Widget> getCircleDotWidgetList(
-      {required int length,
-      required double distance,
-      required double size,
-      required Color color}) {
-    return List.generate(
-        length,
-        (index) => Transform.translate(
-              offset: getOffset(index * (360 / length), distance: distance),
-              child: Container(
-                height: size,
-                width: size,
-                decoration: BoxDecoration(shape: BoxShape.circle, color: color),
-              ),
-            )).toList();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,84 +22,94 @@ class _AnimatedDotsState extends State<AnimatedDots> {
           alignment: Alignment.center,
           children: [
             RotatingAnimation(
-                children: getCircleDotWidgetList(
+                child: DistanceAnimation(
                     length: 13,
                     distance: 25,
                     size: 10,
-                    color: HexColor("780116"))),
+                    distanceEnd: 25 + (25 / 2),
+                    color: HexColor("004b23"))),
             RotatingAnimation(
                 begin: 360,
                 end: 0,
                 duration: const Duration(seconds: 15),
-                children: getCircleDotWidgetList(
+                child: DistanceAnimation(
                     length: 18,
                     distance: 40,
                     size: 10,
-                    color: HexColor("370617"))),
+                    distanceEnd: 40 + (40 / 2),
+                    color: HexColor("006400"))),
             RotatingAnimation(
                 duration: const Duration(seconds: 20),
-                children: getCircleDotWidgetList(
+                child: DistanceAnimation(
                     length: 22,
                     distance: 55,
                     size: 10,
-                    color: HexColor("6a040f"))),
+                    distanceEnd: 55 + (55 / 2),
+                    color: HexColor("007200"))),
             RotatingAnimation(
                 begin: 360,
                 end: 0,
                 duration: const Duration(seconds: 25),
-                children: getCircleDotWidgetList(
+                child: DistanceAnimation(
                     length: 27,
                     distance: 70,
                     size: 10,
-                    color: HexColor("9d0208"))),
+                    distanceEnd: 70 + (70 / 2),
+                    color: HexColor("008000"))),
             RotatingAnimation(
                 duration: const Duration(seconds: 30),
-                children: getCircleDotWidgetList(
+                child: DistanceAnimation(
                     length: 32,
                     distance: 85,
                     size: 10,
-                    color: HexColor("d00000"))),
+                    distanceEnd: 85 + (85 / 2),
+                    color: HexColor("38b000"))),
             RotatingAnimation(
                 begin: 360,
                 end: 0,
                 duration: const Duration(seconds: 35),
-                children: getCircleDotWidgetList(
+                child: DistanceAnimation(
                     length: 37,
                     distance: 100,
                     size: 10,
-                    color: HexColor("dc2f02"))),
+                    distanceEnd: 100 + (100 / 2),
+                    color: HexColor("70e000"))),
             RotatingAnimation(
                 duration: const Duration(seconds: 40),
-                children: getCircleDotWidgetList(
+                child: DistanceAnimation(
                     length: 42,
                     distance: 115,
                     size: 10,
-                    color: HexColor("e85d04"))),
+                    distanceEnd: 115 + (115 / 2),
+                    color: HexColor("9ef01a"))),
             RotatingAnimation(
                 begin: 360,
                 end: 0,
                 duration: const Duration(seconds: 45),
-                children: getCircleDotWidgetList(
+                child: DistanceAnimation(
                     length: 47,
                     distance: 130,
                     size: 10,
-                    color: HexColor("f48c06"))),
+                    distanceEnd: 130 + (130 / 2),
+                    color: HexColor("ccff33"))),
             RotatingAnimation(
                 duration: const Duration(seconds: 50),
-                children: getCircleDotWidgetList(
+                child: DistanceAnimation(
                     length: 52,
                     distance: 145,
                     size: 10,
-                    color: HexColor("faa307"))),
+                    distanceEnd: 145 + (145 / 2),
+                    color: HexColor("D1FF47"))),
             RotatingAnimation(
                 begin: 360,
                 end: 0,
                 duration: const Duration(seconds: 55),
-                children: getCircleDotWidgetList(
+                child: DistanceAnimation(
                     length: 57,
                     distance: 160,
                     size: 10,
-                    color: HexColor("ffba08"))),
+                    distanceEnd: 160 + (160 / 2),
+                    color: HexColor("E0FF85"))),
           ],
         ),
       ),
@@ -130,14 +117,162 @@ class _AnimatedDotsState extends State<AnimatedDots> {
   }
 }
 
+class DistanceAnimation extends StatefulWidget {
+  final int length;
+  final double distance;
+  final double size;
+  final Color color;
+  final double? distanceEnd;
+  const DistanceAnimation({
+    super.key,
+    required this.length,
+    required this.distance,
+    required this.size,
+    required this.color,
+    this.distanceEnd,
+  });
+
+  @override
+  State<DistanceAnimation> createState() => _DistanceAnimationState();
+}
+
+class _DistanceAnimationState extends State<DistanceAnimation>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController animationController;
+  late final Animation animation;
+  late final Animation sizeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 500));
+    animation = Tween<double>(
+            begin: widget.distance, end: widget.distanceEnd ?? widget.distance)
+        .animate(
+            CurvedAnimation(parent: animationController, curve: Curves.linear));
+    sizeAnimation = Tween<double>(begin: widget.size, end: widget.size - 2)
+        .animate(
+            CurvedAnimation(parent: animationController, curve: Curves.linear));
+    animationController.forward();
+    statusListener();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  statusListener() {
+    animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        animationController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        animationController.forward();
+      }
+    });
+  }
+
+  Offset getOffset(double angle, {double? distance}) {
+    final x = (distance ?? 0) * math.cos(angle * (math.pi / 180));
+    final y = (distance ?? 0) * math.sin(angle * (math.pi / 180));
+    return Offset(x, y);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: animationController,
+        child: AnimatedCustomContainer(color: widget.color, size: widget.size),
+        builder: (context, child) {
+          return Stack(
+              children: List.generate(
+                  widget.length,
+                  (index) => Transform.translate(
+                        offset: getOffset(index * (360 / widget.length),
+                            distance: animation.value),
+                        child: child,
+                      )));
+        });
+  }
+}
+
+class AnimatedCustomContainer extends StatefulWidget {
+  const AnimatedCustomContainer({
+    super.key,
+    required this.color,
+    required this.size,
+  });
+
+  final Color color;
+  final double size;
+
+  @override
+  State<AnimatedCustomContainer> createState() =>
+      _AnimatedCustomContainerState();
+}
+
+class _AnimatedCustomContainerState extends State<AnimatedCustomContainer>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController animationController;
+  late final Animation animation;
+  late final Animation sizeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 500));
+
+    sizeAnimation = Tween<double>(begin: widget.size, end: widget.size - 2)
+        .animate(
+            CurvedAnimation(parent: animationController, curve: Curves.linear));
+    animationController.forward();
+    statusListener();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  statusListener() {
+    animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        animationController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        animationController.forward();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: animationController,
+        builder: (context, child) {
+          return Container(
+            height: sizeAnimation.value,
+            width: sizeAnimation.value,
+            decoration:
+                BoxDecoration(shape: BoxShape.circle, color: widget.color),
+          );
+        });
+  }
+}
+
 class RotatingAnimation extends StatefulWidget {
-  final List<Widget> children;
+  final Widget child;
   final double? begin;
   final double? end;
   final Duration? duration;
 
   const RotatingAnimation(
-      {super.key, required this.children, this.begin, this.end, this.duration});
+      {super.key, required this.child, this.begin, this.end, this.duration});
 
   @override
   State<RotatingAnimation> createState() => _RotatingAnimationState();
@@ -169,10 +304,8 @@ class _RotatingAnimationState extends State<RotatingAnimation>
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-      animation: animation,
-      child: Stack(
-        children: widget.children,
-      ),
+      animation: animationController,
+      child: widget.child,
       builder: (context, child) => Transform.rotate(
           angle: animation.value * (math.pi / 180), child: child),
     );
